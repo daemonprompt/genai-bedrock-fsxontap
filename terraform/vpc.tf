681b12aa566bd149719c9c6d4d07b4effe74e717
daemonprompt <daemonprompt@gmail.com>
2025-07-08 20:21:16 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,3 @@
-
 provider "aws" {
   region = var.aws_region
 
@@ -10,19 +9,13 @@ provider "aws" {
 locals {
   tags = {
     project = "genai-bedrock-fsxontap"
-    owner       = "aws"
+    owner   = "aws"
   }
 }
 
 data "aws_availability_zones" "available" {
   state = "available"
-
-  filter {
-    name   = "group-name"
-    values = [var.aws_region]
-  }
 }
-
 
 resource "random_string" "suffix" {
   length  = 8
@@ -33,14 +26,10 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
 
-  name                 = "fsxn-bedrock-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
+  name            = "fsxn-bedrock-vpc"
+  cidr            = var.vpc_cidr
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24"]
+  # ...other module variables...
 }
